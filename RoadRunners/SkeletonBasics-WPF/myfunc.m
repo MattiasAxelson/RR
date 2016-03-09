740e0576f92fa4603636992e18de59277dd19241
johannaalfredsson <johanna.alfredsson@hotmail.com>
2016-03-09 13:28:34 +0100

    kod för lokala max i matlab-delen

--- RoadRunners/SkeletonBasics-WPF/myfunc.m
+++ RoadRunners/SkeletonBasics-WPF/myfunc.m
@@ -1,8 +1,14 @@
 function [x] = myfunc(a) 
 
 %figure (1)
-x = plot (a);
+%x = plot (a);
 %plot(a)
  %hold on
 
+
+[Maxima,MaxIdx] = findpeaks(a);
+aInv = 1.01*max(a) - a;
+[Minima,MinIdx] = findpeaks(aInv);
+Minima = a(MinIdx);
+x = plot(Minima);
 end
