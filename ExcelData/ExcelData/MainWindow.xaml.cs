57c4a8fd4b88748870d4bddb63cebc4bd6e09089
Per Welander <perwelander.swe@gmail.com>
2016-04-14 16:40:12 +0200

    MatteTest

--- ExcelData/ExcelData/MainWindow.xaml.cs
+++ ExcelData/ExcelData/MainWindow.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Office.Interop.Excel;
+using Microsoft.Win32;
+using System.IO;
+using System.Reflection;
+using Excel = Microsoft.Office.Interop.Excel;
+
 
 namespace ExcelData
 {
@@ -26,31 +31,61 @@ namespace ExcelData
             InitializeComponent();
         }
 
+
+
         private void button_Click(object sender, RoutedEventArgs e)
         {
             Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
             app.Visible = true;
             app.WindowState = XlWindowState.xlMaximized;
 
+
+
             Workbook wb = app.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
             Worksheet ws = wb.Worksheets[1];
+            Excel.Range aRange;
             DateTime currentDate = DateTime.Now;
 
-            int[] numbers = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
-            for (int i = 1; i <= numbers.Length; i++)
-                ws.Range["E" + i].Value = numbers[i - 1];
+            // Set the range to fill.
+            aRange = ws.get_Range("A1", "M100");
 
-            ws.Range["A1:A3"].Value = "Who is number one? :)";
-            ws.Range["A4"].Value = "vitoshacademy.com";
-            ws.Range["A5"].Value = currentDate;
-            ws.Range["B6"].Value = "Tommorow's date is: =>";
-            ws.Range["C6"].FormulaLocal = "= A5 + 1";
-            ws.Range["A7"].FormulaLocal = "=SUM(D1:D10)";
-            for (int i = 1; i <= 10; i++)
-                ws.Range["D" + i].Value = i * 2;
 
-            wb.SaveAs("C:\\Users\\Per\\Desktop\\vitoshacademy5.xlsx");
-        
+
+            int[] HB10sec = new int[] { 1, 2, 31, 6, 5, 3213, 7, 8 };
+            int[] Knee10sec = new int[] { 1, 2, 31, 666, 5, 3213, 7, 8 };
+            int[] Hip10sec = new int[] { 1, 2, 31, 3213123, 5, 3213, 7, 8 };
+
+            for (int i = 1; i <= HB10sec.Length; i++)
+            {
+                ws.Range["A0" + (i + 2)].Value = "'" + (i * 10) + "-" + (i + 1) * 10;
+                ws.Range["B0" + (i + 2)].Value = HB10sec[i - 1];
+                ws.Range["C0" + (i + 2)].Value = Knee10sec[i - 1];
+                ws.Range["D0" + (i + 2)].Value = Hip10sec[i - 1];
+
+            }
+
+            string time = DateTime.Now.ToString(@"MM\/dd\/yyyy HH:mm tt");
+            string Namn = "Ano Nym";
+
+
+            ws.Range["E1"].Value = "Namn: " + Namn;
+            ws.Range["F1"].Value = "Date and Time: " + time;
+            ws.Range["A2"].Value = "Intervall [sec]";
+            ws.Range["B2"].Value = "HeartBeat";
+            ws.Range["C2"].Value = "Angle Hip";
+            ws.Range["D2"].Value = "Angle Knee";
+
+            aRange.Columns.AutoFit();
+            aRange.EntireColumn.AutoFit();
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+                wb.SaveAs(saveFileDialog.FileName);
         }
-    }
-}
+
+
+
+    } }
+        
+        
+  
