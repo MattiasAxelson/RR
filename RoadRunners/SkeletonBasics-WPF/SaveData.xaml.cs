5a08697e9dffbcdfb7f075633e782cfbeb07bd3e
Per Welander <perwelander.swe@gmail.com>
2016-04-21 08:38:36 +0200

    Nu ska vi merga

--- RoadRunners/SkeletonBasics-WPF/SaveData.xaml.cs
+++ RoadRunners/SkeletonBasics-WPF/SaveData.xaml.cs
@@ -224,7 +224,7 @@ namespace Microsoft.Samples.Kinect.SkeletonBasics
                 
                  if (ExcelVelocityList != null)
                  {
-                    ws.Range["E0" + (i + 3)].Value = ExcelVelocityList[i];
+                   // ws.Range["E0" + (i + 3)].Value = ExcelVelocityList[i];
 
                  }
             }
