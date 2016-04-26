4a499c0f8ac89178ad5cc83daf1e38129c8cd720
Gunnarskog <jesperalfreus@gmail.com>
2016-04-26 08:43:45 +0200

    Ta inget för fan, kanske lite matlab

--- RoadRunners/SkeletonBasics-WPF/myfunc.m
+++ RoadRunners/SkeletonBasics-WPF/myfunc.m
@@ -7,12 +7,21 @@ h = figure(1); set(gcf,'visible','off')
 
 h.PaperUnits = 'inches';
 h.PaperPosition = [0 0 14 4];
-length(a)
-length(b)
-length(c)
-length(d)
+
 
 x = plot (a,b,a,c,a,d);
+% aa=[a;a];
+% bb=[b;b];
+% zz=zeros(size(aa));
+% 
+% hs=surf(aa,bb,zz,bb, 'EdgeColor', 'interp')
+% colormap(flipud(autumn))
+% view(2)
+% 
+% HANDLE = gca;
+% get(HANDLE);
+% set(HANDLE, 'Color', [0,0.1,0.3]);
+% hs.LineWidth=2
 title('SuperGrafen');
 xlabel('Tid');
 ylabel('Vinkel');
